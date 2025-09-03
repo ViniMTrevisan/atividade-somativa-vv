311e40a05631ad57587805afcd5772e8a12c26d1
GuiMarafon <guilherme.marafon@pucpr.edu.br>
2025-09-03 20:20:16 -0300

    solucoes.py

--- solucoes.py
+++ solucoes.py
@@ -1,6 +1,16 @@
 def sao_anagramas(string1, string2):
-    #TODO: implementar logica
-    pass
+    #TODO:
+    s1 = string1.replace(" ", "").lower()
+    s2 = string2.replace(" ", "").lower()
+
+    if len(s1) != len(s2):
+        return False
+
+    return sorted(s1) == sorted(s2)
+print(sao_anagramas("amor", "roma"))
+print(sao_anagramas("A gentleman", "Elegant man"))
+print(sao_anagramas("gato", "cabra"))
+pass
 
 def cifra_de_cesar(texto, deslocamento):
     #TODO: implementar logica
